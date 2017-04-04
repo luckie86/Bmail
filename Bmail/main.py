2e91e67ca92417b073160c7818938bb5ee683361
Luka Brinar <luckie.luke@gmail.com>
2017-04-04 11:22:51 +0200

    added create acc, login

--- Bmail/main.py
+++ Bmail/main.py
@@ -3,6 +3,10 @@ import os
 import jinja2
 import webapp2
 import cgi
+import json
+import hmac
+
+from google.appengine.api import urlfetch
 
 from google.appengine.api import users
 
@@ -30,13 +34,72 @@ class BaseHandler(webapp2.RequestHandler):
         template = jinja_env.get_template(view_filename)
         return self.response.out.write(template.render(params))
 
+    def display_message(self, message):
+        params = {'message': message}
+        self.render_template('message.html', params)
+
 class CreateAccountHandler(BaseHandler):
     def get(self):
-        return self.render_template("create-account.html")
+        # 1 Preveri da uporabnik z emailom ne obstaja.
+        user_name = cgi.escape(self.request.get('username'))
+        email = cgi.escape(self.request.get('email'))
+        name = cgi.escape(self.request.get('name'))
+        password = cgi.escape(self.request.get('password'))
+        last_name = cgi.escape(self.request.get('lastname'))
+
+        user_from_base = User.query(User.email == email).fetch()
+        user_from_base_email = user_from_base.email()
+        if user_from_base_email == email:
+            message = {"message": "User with %s already exists" % email}
+            return self.display_message(message=message)
+        # 2 Ustvari uporabnika.
+        else:
+            password_hash = hmac.new(password).hexdigest()
+            save_user = User(name=name, last_name=last_name, user_name=user_name, password=password_hash)
+            save_user.put()
+            return self.redirect("index.html")
+
+    def post(self):
+        # 1 Preveri da uporabnik z emailom ne obstaja.
+        user_name = cgi.escape(self.request.get('username'))
+        email = cgi.escape(self.request.get('email'))
+        name = cgi.escape(self.request.get('name'))
+        password = cgi.escape(self.request.get('password'))
+        last_name = cgi.escape(self.request.get('lastname'))
+
+        user_from_base = User.query(User.email == email).fetch()
+        user_from_base_email = user_from_base.email()
+        if user_from_base_email == email:
+            message = {"message": "User with %s already exists" % email}
+            return self.display_message(message=message)
+        # 2 Ustvari uporabnika.
+        else:
+            password_hash = hmac.new(password).hexdigest()
+            save_user = User(name=name, last_name=last_name, user_name=user_name, password=password_hash)
+            save_user.put()
+            return self.redirect("index.html")
+
 
 class LoginHandler(BaseHandler):
     def get(self):
         return self.render_template("index.html")
+
+    def post(self):
+        email = cgi.escape(self.request.get('email'))
+        password = cgi.escape(self.request.get('password'))
+        password_hash = hmac.new(password).hexdigest()
+        users = User.query(User.email == email).fetch()
+        if len(users) == 0:
+            message = {"message": "There is no user with %s" % email}
+            return self.display_message(message=message)
+        else:
+            user = users[0]
+            if user.password != password_hash:
+                message = {"message": "Wrong password"}
+                return self.display_message(message=message)
+
+        return self.redirect("/home")
+
 
 class HomeHandler(BaseHandler):
     def get(self):
@@ -52,7 +115,10 @@ class SentHandler(BaseHandler):
 
 class WeatherHandler(BaseHandler):
     def get(self):
-        return self.render_template("weather.html")
+        url = "http://api.openweathermap.org/data/2.5/weather?q=Celje,si&APPID=0fa4f697d72b1a4616a02c99f798df9c"
+        data = urlfetch.fetch(url).content
+        json_data = json.loads(data)
+        return self.render_template("weather.html", json_data)
 
 app = webapp2.WSGIApplication([
     webapp2.Route('/', LoginHandler),
--- Bmail/models.py
+++ Bmail/models.py
@@ -5,13 +5,11 @@ class Email(ndb.Model):
     receiver = ndb.StringProperty()
     subject = ndb.StringProperty()
     email = ndb.TextProperty()
-#sent = ndb.BooleanProperty()
-#received = ndb.BooleanProperty()
     time = ndb.DateTimeProperty(auto_now_add=True)
 
 class User(ndb.Model):
     name = ndb.StringProperty()
-    lastname = ndb.StringProperty()
-    username = ndb.StringProperty()
+    last_name = ndb.StringProperty()
+    user_name = ndb.StringProperty()
     email = ndb.StringProperty()
     password = ndb.StringProperty()
