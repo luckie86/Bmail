c4b6d2e632799a794821be5258ab381e814eab98
Luka Brinar <luckie.luke@gmail.com>
2017-04-05 13:33:48 +0200

    added Google login

--- Bmail/main.py
+++ Bmail/main.py
@@ -2,15 +2,14 @@
 import os
 import jinja2
 import webapp2
-import cgi
 import json
-import hmac
+import cgi
 
 from google.appengine.api import urlfetch
 
 from google.appengine.api import users
 
-from models import Email, User
+from models import Email
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
@@ -31,79 +30,44 @@ class BaseHandler(webapp2.RequestHandler):
     def render_template(self, view_filename, params=None):
         if not params:
             params = {}
+
+        user = users.get_current_user()
+        params["user"] = user
+
+        if user:
+            logged_in = True
+            logout_url = users.create_logout_url('/')
+            params["logout_url"] = logout_url
+        else:
+            logged_in = False
+            login_url = users.create_login_url('/')
+            params["login_url"] = login_url
+
+        params["logged_in"] = logged_in
+
         template = jinja_env.get_template(view_filename)
         return self.response.out.write(template.render(params))
 
-    def display_message(self, message):
-        params = {'message': message}
-        self.render_template('message.html', params)
 
-class CreateAccountHandler(BaseHandler):
-    def get(self):
-        # 1 Preveri da uporabnik z emailom ne obstaja.
-        user_name = cgi.escape(self.request.get('username'))
-        email = cgi.escape(self.request.get('email'))
-        name = cgi.escape(self.request.get('name'))
-        password = cgi.escape(self.request.get('password'))
-        last_name = cgi.escape(self.request.get('lastname'))
-
-        user_from_base = User.query(User.email == email).fetch()
-        user_from_base_email = user_from_base.email()
-        if user_from_base_email == email:
-            message = ("User with %s already exists" % email)
-            return self.display_message(message=message)
-        # 2 Ustvari uporabnika.
-        else:
-            password_hash = hmac.new(password).hexdigest()
-            save_user = User(name=name, last_name=last_name, user_name=user_name, email=email, password=password_hash)
-            save_user.put()
-        return self.redirect("index.html")
-
-    def post(self):
-        # 1 Preveri da uporabnik z emailom ne obstaja.
-        user_name = cgi.escape(self.request.get('username'))
-        email = cgi.escape(self.request.get('email'))
-        name = cgi.escape(self.request.get('name'))
-        password = cgi.escape(self.request.get('password'))
-        last_name = cgi.escape(self.request.get('lastname'))
-
-        user_from_base = User.query(User.email == email).fetch()
-        user_from_base_email = user_from_base.email()
-        if user_from_base_email == email:
-            message = ("User with %s already exists" % email)
-            return self.display_message(message=message)
-        # 2 Ustvari uporabnika.
-        else:
-            password_hash = hmac.new(password).hexdigest()
-            save_user = User(name=name, last_name=last_name, user_name=user_name, email=email, password=password_hash)
-            save_user.put()
-            return self.redirect("index.html")
-
-
-class LoginHandler(BaseHandler):
+class MainHandler(BaseHandler):
     def get(self):
         return self.render_template("index.html")
 
-    def post(self):
-        email = cgi.escape(self.request.get('email'))
-        password = cgi.escape(self.request.get('password'))
-        password_hash = hmac.new(password).hexdigest()
-        users = User.query(User.email == email).fetch()
-        if len(users) == 0:
-            message = ("There is no user with %s email" % email)
-            return self.display_message(message=message)
-        else:
-            user = users[0]
-            if user.password != password_hash:
-                message = ("Wrong password")
-                return self.display_message(message=message)
 
-        return self.redirect("/home")
-
-
-class HomeHandler(BaseHandler):
+class LoadingHandler(BaseHandler):
     def get(self):
-        return self.render_template("home.html")
+        return self.render_template("loading.html")
+
+
+class NewEmailHandler(BaseHandler):
+    def post(self):
+        sender = self.request.get("sender")
+        receiver = cgi.escape(self.request.get("receiver"))
+        subject = cgi.escape(self.request.get("subject"))
+        email = cgi.escape(self.request.get("email"))
+        save_email = Email(sender=sender, receiver=receiver, subjec=subject, email=email)
+        save_email.put()
+        return self.redirect("/received")
 
 class ReceivedHandler(BaseHandler):
     def get(self):
@@ -114,11 +78,13 @@ class ReceivedHandler(BaseHandler):
             params = {"emails": emails}
             return self.render_template("received.html", params)
 
+
 class EachReceivedEmailHandler(BaseHandler):
     def get(self, email_id):
         email = Email.get_by_id(int(email_id))
         params = {"email": email}
         return self.render_template("received-details.html", params)
+
 
 class SentHandler(BaseHandler):
     def get(self):
@@ -129,11 +95,13 @@ class SentHandler(BaseHandler):
             params = {"emails": emails}
             return self.render_template("sent.html", params)
 
+
 class EachSentEmailHandler(BaseHandler):
     def get(self, email_id):
         email = Email.get_by_id(int(email_id))
         params = {"email": email}
         return self.render_template("sent-details.html", params)
+
 
 class WeatherHandler(BaseHandler):
     def get(self):
@@ -144,9 +112,9 @@ class WeatherHandler(BaseHandler):
         return self.render_template("weather.html", params)
 
 app = webapp2.WSGIApplication([
-    webapp2.Route('/', LoginHandler),
-    webapp2.Route('/home', HomeHandler),
-    webapp2.Route('/create-account', CreateAccountHandler),
+    webapp2.Route('/', NewEmailHandler),
+    webapp2.Route('/', MainHandler),
+    webapp2.Route('/loading', LoadingHandler),
     webapp2.Route('/received', ReceivedHandler),
     webapp2.Route('/received-details/<email_id:\d+>', EachReceivedEmailHandler),
     webapp2.Route('/sent', SentHandler),
--- Bmail/models.py
+++ Bmail/models.py
@@ -6,10 +6,3 @@ class Email(ndb.Model):
     subject = ndb.StringProperty()
     email = ndb.TextProperty()
     time = ndb.DateTimeProperty(auto_now_add=True)
-
-class User(ndb.Model):
-    name = ndb.StringProperty()
-    last_name = ndb.StringProperty()
-    user_name = ndb.StringProperty()
-    email = ndb.StringProperty()
-    password = ndb.StringProperty()
